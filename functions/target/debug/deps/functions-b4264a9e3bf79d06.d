c442b68f15c6813555249c1bb7917918068971c2
julianParrax <julianfranco.parra@gmail.com>
2022-12-02 22:05:11 -0300

    Se agrega functions

---
+++ functions/target/debug/deps/functions-b4264a9e3bf79d06.d
@@ -0,0 +1,5 @@
+c:\Users\julian\Desktop\RUST\RUST-topics-githubRepo\RUST-topics\functions\target\debug\deps\functions-b4264a9e3bf79d06.rmeta: src\main.rs
+
+c:\Users\julian\Desktop\RUST\RUST-topics-githubRepo\RUST-topics\functions\target\debug\deps\functions-b4264a9e3bf79d06.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/deps/functions-c83a1c8f4c43d78c.d
@@ -0,0 +1,5 @@
+c:\Users\julian\Desktop\RUST\RUST-topics-githubRepo\RUST-topics\functions\target\debug\deps\functions-c83a1c8f4c43d78c.rmeta: src\main.rs
+
+c:\Users\julian\Desktop\RUST\RUST-topics-githubRepo\RUST-topics\functions\target\debug\deps\functions-c83a1c8f4c43d78c.d: src\main.rs
+
+src\main.rs:
